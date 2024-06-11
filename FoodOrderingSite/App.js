3bfdf142a03d150487cdbef4d263aae916f559e7
joshiriya0110 <joshiriya0110@gmail.com>
2024-06-11 19:28:50 +0530

    written react components

--- FoodOrderingSite/App.js
+++ FoodOrderingSite/App.js
@@ -1,6 +1,36 @@
 import React from "react";
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom";
 
-const jsxHeading = <h1 id="heading">Heading using jsx</h1>
+
+//React Element
+const jsxHeading = (
+  <div>
+    <h1 className="head">Heading using jsx</h1>
+    <h2>Second heading</h2>
+  </div>
+);
+
+
+//React Component
+//Class Based Component - OLD
+//Functional Component - NEW
+
+const Title = () =>{
+    return(
+        <h1>Title</h1>
+    )
+}
+
+const HeadingComponent = () =>{
+    return (
+        <div>
+            <Title/>
+            <h1>Functional Component</h1>
+        </div>
+    );
+}
+
+
+//Rendering functional component
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent/>);
