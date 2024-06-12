163a6c636cc1a2fe65565459810c4ad0fb79c491
joshiriya0110 <joshiriya0110@gmail.com>
2024-06-12 08:39:19 +0530

    dynamically inserting reestaurant cards, using the data from swiggy

--- FoodOrderingSite/App.js
+++ FoodOrderingSite/App.js
@@ -16,67 +16,264 @@ import ReactDOM from "react-dom";
  * ---Contact
  */
 
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img src="" alt="Logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const Header = () =>{
-    return(
-        <div className="header">
-            <div className="logo-container">
-                <img src="" alt="Logo"/>
-            </div>
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
-                </ul>
+const resObj = [
+  {
+    id: "816059",
+    name: "Salad Days",
+    cloudinaryImageId: "3a6b427c050f6a0ca7c12fea2d6aa383",
+    locality: "Electronic City",
+    areaName: "Electronic City",
+    costForTwo: "₹500 for two",
+    cuisines: ["Salads"],
+    avgRating: 4.2,
+    parentId: "796",
+    avgRatingString: "4.2",
+    totalRatingsString: "10+",
+    sla: {
+      deliveryTime: 34,
+      lastMileTravel: 0.9,
+      serviceability: "SERVICEABLE",
+      slaString: "30-35 mins",
+      lastMileTravelString: "0.9 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2024-06-13 01:00:00",
+      opened: true,
+    },
+    badges: {
+      imageBadges: [
+        {
+          imageId: "newg.png",
+          description: "Gourmet",
+        },
+      ],
+    },
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {
+          badgeObject: [
+            {
+              attributes: {
+                description: "Gourmet",
+                imageId: "newg.png",
+              },
+            },
+          ],
+        },
+        textBased: {},
+        textExtendedBadges: {},
+      },
+    },
+    aggregatedDiscountInfoV3: {
+      header: "40% OFF",
+      subHeader: "UPTO ₹80",
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    isNewlyOnboarded: true,
+    restaurantOfferPresentationInfo: {},
+  },
+  {
+    id: "816059",
+    name: "KFC",
+    cloudinaryImageId: "3a6b427c050f6a0ca7c12fea2d6aa383",
+    locality: "Electronic City",
+    areaName: "Electronic City",
+    costForTwo: "₹500 for two",
+    cuisines: ["Salads"],
+    avgRating: 4.2,
+    parentId: "796",
+    avgRatingString: "4.2",
+    totalRatingsString: "10+",
+    sla: {
+      deliveryTime: 34,
+      lastMileTravel: 0.9,
+      serviceability: "SERVICEABLE",
+      slaString: "30-35 mins",
+      lastMileTravelString: "0.9 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2024-06-13 01:00:00",
+      opened: true,
+    },
+    badges: {
+      imageBadges: [
+        {
+          imageId: "newg.png",
+          description: "Gourmet",
+        },
+      ],
+    },
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {
+          badgeObject: [
+            {
+              attributes: {
+                description: "Gourmet",
+                imageId: "newg.png",
+              },
+            },
+          ],
+        },
+        textBased: {},
+        textExtendedBadges: {},
+      },
+    },
+    aggregatedDiscountInfoV3: {
+      header: "40% OFF",
+      subHeader: "UPTO ₹80",
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    isNewlyOnboarded: true,
+    restaurantOfferPresentationInfo: {},
+  },
+  {
+    id: "816059",
+    name: "Subway",
+    cloudinaryImageId: "3a6b427c050f6a0ca7c12fea2d6aa383",
+    locality: "Electronic City",
+    areaName: "Electronic City",
+    costForTwo: "₹500 for two",
+    cuisines: ["Salads"],
+    avgRating: 4.2,
+    parentId: "796",
+    avgRatingString: "4.2",
+    totalRatingsString: "10+",
+    sla: {
+      deliveryTime: 34,
+      lastMileTravel: 0.9,
+      serviceability: "SERVICEABLE",
+      slaString: "30-35 mins",
+      lastMileTravelString: "0.9 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2024-06-13 01:00:00",
+      opened: true,
+    },
+    badges: {
+      imageBadges: [
+        {
+          imageId: "newg.png",
+          description: "Gourmet",
+        },
+      ],
+    },
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {
+          badgeObject: [
+            {
+              attributes: {
+                description: "Gourmet",
+                imageId: "newg.png",
+              },
+            },
+          ],
+        },
+        textBased: {},
+        textExtendedBadges: {},
+      },
+    },
+    aggregatedDiscountInfoV3: {
+      header: "40% OFF",
+      subHeader: "UPTO ₹80",
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    isNewlyOnboarded: true,
+    restaurantOfferPresentationInfo: {},
+  },
+];
 
-            </div>
+const RestaurantCard = (props) => {
+  console.log(props);
+  const { resData } = props;
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/27d43f8a2c1ca62704bb2dfd88d5b268"
+      />
+      <h3>{resData.name}</h3>
+      <h4>{resData.cuisines.join(",")}</h4>
+      <h4>{resData.avgRating}</h4>
+      <h4>30 minutes</h4>
+    </div>
+  );
+};
 
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard resData={resObj[0]} />
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-const RestaurantCard = (props) =>{
-    return(
-        <div className="res-card">
-            <img className="res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/27d43f8a2c1ca62704bb2dfd88d5b268"/>
-            <h3>{props.resName}</h3>  
-            <h4>{props.cuisine}</h4>  
-            <h4>4.5 stars</h4>
-            <h4>38 minutes</h4>        
-
-
-        </div>
-    )
-}
-
-const Body = () =>{
-    return(
-        <div className="body">
-            <div className="search">
-                Search
-            </div>
-            <div className="res-container">
-                <RestaurantCard resName="Meghana Foods"  cuisine="Indian, Chinese, American"/>
-                <RestaurantCard resName="KFC" cuisine="Italian, American"/>
-                <RestaurantCard resName="Subway" cuisine="Tibetan, Chinese"/>
-                <RestaurantCard resName="Udupi" cuisine="Nepalese, Indian"/>
-                <RestaurantCard resName="BLR" cuisine="Indian, Chinese, American"/>
-            </div>
-        </div>
-    )
-}
-
-
-const AppLayout = () =>{
-    return(
-        <div className="app">
-            <Header/>
-            <Body/>
-        </div>
-    )
-}
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>)
+root.render(<AppLayout />);
